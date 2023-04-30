370c5496d25c91621ed318f560d036d53f632a45
jakaria455173 <jakaria455173@gmail.com>
2023-05-01 04:14:38 +0600

    service card styles

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,23 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import "./services.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 const Services = () => {
   const [servicesData, setServicesData] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/allData")
-      .then((response) => response.json())
-      .then((data) => setServicesData(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/data")
+  //     .then((response) => response.json())
+  //     .then((data) => setServicesData(data.services));
+  // }, []);
 
-  console.log(servicesData);
+  // console.log(servicesData);
   return (
-    <div>
+    <>
       <h1>Our Services</h1>
       {/* <div className="services">
-        {servicesData[0]?.services?.map((service) => (
-          <div key={service?.id} className="service-card">
+        {servicesData.map((service) => (
+          <div key={service?.id} className="service-card d-flex flex-column">
+            <img className="w-100 my-1 img-thumbnail" src={service.image} alt="" />
             <h2>{service?.name}</h2>
             <p>{service?.description}</p>
             <ul>
@@ -28,14 +29,14 @@ const Services = () => {
               ))}
             </ul>
             <p>Price: ${service?.price}</p>
-            <p>Duration: {service?.duration}</p>
-            <Link to={`/services/${service?.id}`}>
-              <button>Details</button>
+            <p className="mb-2">Duration: {service?.duration}</p>
+            <Link className="mt-auto" to={`/services/${service?.id}`}>
+              <button className="btn fw-semibold btn-outline-danger rounded-1">Details</button>
             </Link>
           </div>
         ))}
       </div> */}
-    </div>
+    </>
   );
 };
 
